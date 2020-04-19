56bc5ba07df97b6f65f7a5673376e75531657500
bigguscodus <mikka_tolmachev@protonmail.com>
2020-04-19 15:55:45 +0300

    BED-1 add concept file

---
+++ bedtools.py
@@ -0,0 +1,15 @@
+import argparse
+parser = argparse.ArgumentParser()
+class Bedtools():
+    def __init__(self):
+        pass
+    def sort(self):
+        pass
+    def substract(self):
+        pass
+    def merge(self):
+        pass
+    def intersect(self):
+        pass
+    def getfasta(self):
+        pass
