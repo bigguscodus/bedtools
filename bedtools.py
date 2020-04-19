1772bfe6e15403f10b54a48ede1282c3537d5a1f
bigguscodus <mikka_tolmachev@protonmail.com>
2020-04-19 18:45:45 +0300

    BED-3 add sort method

--- bedtools.py
+++ bedtools.py
@@ -7,6 +7,8 @@ class Bedtools:
     def __init__(self):
         pass
     def _read_bed(self, first_bed = args.first_bed, second_bed = args.second_bed):
+        self.first_path = first_bed
+        self.second_path = second_bed
         if (first_bed is not None):
             with open(first_bed) as file_1:
                 self.first_bed_generator = (str(line).strip().split("\t") for line in file_1.readlines()[1:])
@@ -17,11 +19,14 @@ class Bedtools:
                 self.second_bed_generator = (str(line).strip().split("\t") for line in file_2.readlines()[1:])
         else:
             self.second_bed_generator = None
-        print(self.first_bed_generator)
-        print(self.second_bed_generator)
-        #print(self.second_bed_generator)
-    def sort(bed_to_sort = args.first_bed):
-        pass
+    def sort(self):
+        if (self.first_bed_generator is None):
+            raise ValueError("Need .bed file for sorting")
+        name = f"sorted_{self.first_path.split('/')[-1]}"
+        with open(name,"w+") as sorted_out:
+            for read in sorted(self.first_bed_generator, key=lambda x: (x[0], int(x[1]))):
+                read = "\t".join(read)
+                sorted_out.write(f"{read}\n")
     def substract(self):
         pass
     def merge(self):
@@ -33,3 +38,4 @@ class Bedtools:
 if __name__ == "__main__":
     bedtools = Bedtools()
     bedtools._read_bed()
+    bedtools.sort()
