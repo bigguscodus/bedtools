2482d32ea046824cb71ff122756a6f60a3ca3cf9
bigguscodus <mikka_tolmachev@protonmail.com>
2020-04-19 17:07:16 +0300

    BED-2 add bedtools input

--- bedtools.py
+++ bedtools.py
@@ -1,9 +1,26 @@
 import argparse
 parser = argparse.ArgumentParser()
-class Bedtools():
+parser.add_argument("-fb", "--first_bed", help="Path to the first bed file")
+parser.add_argument("-sb", "--second_bed", help="path to the second bed file")
+args = parser.parse_args()
+class Bedtools:
     def __init__(self):
         pass
-    def sort(self):
+    def _read_bed(self, first_bed = args.first_bed, second_bed = args.second_bed):
+        if (first_bed is not None):
+            with open(first_bed) as file_1:
+                self.first_bed_generator = (str(line).strip().split("\t") for line in file_1.readlines()[1:])
+        else:
+            self.first_bed_generator = None
+        if (second_bed is not None):
+            with open(second_bed) as file_2:
+                self.second_bed_generator = (str(line).strip().split("\t") for line in file_2.readlines()[1:])
+        else:
+            self.second_bed_generator = None
+        print(self.first_bed_generator)
+        print(self.second_bed_generator)
+        #print(self.second_bed_generator)
+    def sort(bed_to_sort = args.first_bed):
         pass
     def substract(self):
         pass
@@ -13,3 +30,6 @@ class Bedtools():
         pass
     def getfasta(self):
         pass
+if __name__ == "__main__":
+    bedtools = Bedtools()
+    bedtools._read_bed()
