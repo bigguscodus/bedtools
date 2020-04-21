c290611c307534182b8edfda664962910caa7396
bigguscodus <mikka_tolmachev@protonmail.com>
2020-04-21 13:17:58 +0300

    BED-9 add new structure for tests

---
+++ tests/test_bedtools.py
@@ -0,0 +1,25 @@
+from bedtools import Bedtools
+import unittest
+import hashlib
+bedtools = Bedtools()
+class TestBedtools(unittest.TestCase):
+    def multi_test(self, filename):
+        h = hashlib.sha256()
+        b = bytearray(128 * 1024)
+        mv = memoryview(b)
+        with open(filename, 'rb', buffering=0) as f:
+            for n in iter(lambda: f.readinto(mv), 0):
+                h.update(mv[:n])
+        return h.hexdigest()
+    def test_sort(self,expected = "bedtools_sorted.bed",observed = "sorted_lamina.bed"):
+        bedtools.sort(bed_path="lamina.bed")
+        self.assertEqual(self.multi_test(filename=expected), self.multi_test(filename=observed))
+    def test_merge(self, expected = "bedtools_merge.bed", observed = "merged_lamina_m.bed"):
+        bedtools.merge(bed_path="lamina_m.bed")
+        self.assertEqual(self.multi_test(filename=expected), self.multi_test(filename=observed))
+    def test_getfasta(self,expected = "bedtools_getfasta.fa", observed = "fasta.fa"):
+        bedtools.getfasta(path_to_fasta="sequence.fasta",
+                          path_to_bed="sample.bed")
+        self.assertEqual(self.multi_test(filename=expected), self.multi_test(filename=observed))
+if __name__ == '__main__':
+    unittest.main()
